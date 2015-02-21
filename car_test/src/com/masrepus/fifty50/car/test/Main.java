ade4a1f8350d6dfa18a179d7b8e7359f44abebf4
Samuel <masrepus97@gmail.com>
2015-02-21 16:03:38 +0100

    restart server when client disconnects

--- car_test/src/com/masrepus/fifty50/car/test/Main.java
+++ car_test/src/com/masrepus/fifty50/car/test/Main.java
@@ -1,6 +1,4 @@
-package com.masrepus.fifty50.car;
-
-import com.pi4j.io.gpio.*;
+package com.masrepus.fifty50.car.test;
 
 import java.io.*;
 import java.net.ServerSocket;
@@ -15,10 +13,7 @@ public class Main {
     public static enum Speed {SLOW, FAST}
     public static enum Mode {FORWARD, BACKWARD, LEFT, RIGHT}
 
-    private GpioController gpio;
-    private GpioPinDigitalOutput forward_fast, backward_fast, left_fast, right_fast;
-    private GpioPinDigitalOutput forward_slow, backward_slow, left_slow, right_slow;
-
+    private ServerSocket serverSocket;
     private boolean finish = false;
 
     public static void main(String[] args) {
@@ -83,23 +78,7 @@ public class Main {
         main.stop();
     }
 
-    public Main() {
-        gpio = GpioFactory.getInstance();
-
-        //provision gpio pins 0 to 3 as output pins for fast/strong commands
-        forward_fast = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_00, "Forward fast", PinState.LOW);
-        backward_fast = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_07, "Backward fast", PinState.LOW);
-        left_fast = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_02, "Left strong", PinState.LOW);
-        right_fast = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_03, "Right strong", PinState.LOW);
-
-        //provision gpio 4 to 7 as output pins for slow/light commands
-        forward_slow = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_04, "Forward slow", PinState.LOW);
-        backward_slow = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_05, "Backward slow", PinState.LOW);
-        left_slow = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_06, "Left slow", PinState.LOW);
-        right_slow = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "Right slow", PinState.LOW);
-    }
-
-	private void startServer(int port) {
+    private void startServer(int port) {
         try {
             new Server(this, port).start();
         } catch (IOException e) {
@@ -113,16 +92,11 @@ public class Main {
 
     private void forward(Speed speed) {
         String intensity = "";
-        //turn on the correct forward pin
         switch (speed) {
             case SLOW:
-                forward_slow.high();
-                forward_fast.low();
                 intensity = "schwach";
                 break;
             case FAST:
-                forward_fast.high();
-                forward_slow.low();
                 intensity = "stark";
                 break;
         }
@@ -131,16 +105,11 @@ public class Main {
 
     private void backward(Speed speed) {
         String intensity = "";
-        //turn on the correct backward pin
         switch (speed) {
             case SLOW:
-                backward_slow.high();
-                backward_fast.low();
                 intensity = "schwach";
                 break;
             case FAST:
-                backward_fast.high();
-                backward_slow.low();
                 intensity = "stark";
                 break;
         }
@@ -149,16 +118,11 @@ public class Main {
 
     private void left(Speed speed) {
         String intensity = "";
-        //turn on the correct left pin
         switch (speed) {
             case SLOW:
-                left_slow.high();
-                left_fast.low();
                 intensity = "schwach";
                 break;
             case FAST:
-                left_fast.high();
-                left_slow.low();
                 intensity = "stark";
                 break;
         }
@@ -167,16 +131,11 @@ public class Main {
 
     private void right(Speed speed) {
         String intensity = "";
-        //turn on the correct right pin
         switch (speed) {
             case SLOW:
-                right_slow.high();
-                right_fast.low();
                 intensity = "schwach";
                 break;
             case FAST:
-                right_fast.high();
-                right_slow.low();
                 intensity = "stark";
                 break;
         }
@@ -184,22 +143,10 @@ public class Main {
     }
 
     private void brake() {
-        //turn off all accelerating pins
-        forward_slow.low();
-        forward_fast.low();
-
-        backward_slow.low();
-        backward_fast.low();
         System.out.println("Bremse...");
     }
 
     private void straight() {
-        //turn off all steering pins
-        right_slow.low();
-        right_fast.low();
-
-        left_slow.low();
-        left_fast.low();
         System.out.println("Lenken beendet");
     }
 
